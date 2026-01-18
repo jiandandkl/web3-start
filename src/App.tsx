f673958bf5b11443607903ccf9d538b8376ebd5c
dujun <emolingzhu@126.com>
2026-01-18 10:53:42 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   const [showSelectChain, setShowSelectChain] = useState(false);
   const { address } = useWalletStore();
 
+
   return (
     <div className="grid w-screen grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex w-full flex-col gap-[32px] row-start-2 justify-center items-center sm:items-start">
